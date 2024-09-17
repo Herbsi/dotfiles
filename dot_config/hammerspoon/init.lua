e35aa7dc6e86b8d77cc860a38e96198fcc6bc081
Herbsi <git@hoehenberger.me>
2024-09-17 16:38:46 +0200

    hammerspoon: Delete configuration

--- dot_config/hammerspoon/init.lua
+++
@@ -1,8 +0,0 @@
-require "hyper"
-
--- Keybindings for German Quotes
--- Opening Double Quote
-hs.hotkey.bind({"ctrl", "shift"}, '[', function() hs.eventtap.keyStrokes("„") end, nil, nil)
-
--- Opening Single Quote
-hs.hotkey.bind({"ctrl", "shift"}, ']', function() hs.eventtap.keyStrokes("‚") end, nil, nil)
