4f42d8fe1cbbfb07a2f6b5e10e7c933ba0f5e3d2
Herwig Höhenberger <herwig.hoehenberger@valucor.ch>
2025-07-24 07:50:14 +0200

    Add Microsoft.PowerShell_profile.ps1

---
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+﻿
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+Invoke-Expression (&starship init powershell)
+function Set-TerminalTitle {
+    $host.UI.RawUI.WindowTitle = "$(Get-Location)"
+}
