a0f4718295efbeff74298b690025dbb1c5115a96
Herwig Höhenberger <herwig.hoehenberger@valucor.ch>
2025-08-05 07:49:46 +0200

    powershell: Add jj completion

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
-﻿
-Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+﻿Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+Invoke-Expression (& { (jj util completion power-shell | Out-String) })
 Invoke-Expression (&starship init powershell)
 function Set-TerminalTitle {
     $host.UI.RawUI.WindowTitle = "$(Get-Location)"
