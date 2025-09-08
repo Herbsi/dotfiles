990758bb3b3c50b4c8883a3ec450f9395355ecf5
Herwig Höhenberger <herwig.hoehenberger@valucor.ch>
2025-09-08 16:22:33 +0200

    Add install instructions for Windows

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,6 @@
-﻿Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
-Invoke-Expression (& { (jj util completion power-shell | Out-String) })
+﻿Invoke-Expression (& { (jj util completion power-shell | Out-String) })
 Invoke-Expression (&starship init powershell)
 function Set-TerminalTitle {
     $host.UI.RawUI.WindowTitle = "$(Get-Location)"
 }
-Invoke-Expression "$(direnv hook pwsh)"
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
