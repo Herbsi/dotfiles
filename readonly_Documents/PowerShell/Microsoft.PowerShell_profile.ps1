56ca98394b711447994981a7d2f5c2131192a689
Herbsi <git@hoehenberger.ch>
2026-03-31 09:07:18 +0200

    PowerShell: Use template and deploy to $PROFILE

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-﻿Invoke-Expression (& { (jj util completion power-shell | Out-String) })
-Invoke-Expression (&starship init powershell)
-function Set-TerminalTitle {
-    $host.UI.RawUI.WindowTitle = "$(Get-Location)"
-}
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
