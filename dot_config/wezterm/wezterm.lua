cb5f2ae65af0823e20c7872cd5917be3993409c5
Herbsi <git@hoehenberger.ch>
2026-04-27 12:16:24 +0200

    Add wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.initial_cols = 120
+config.initial_rows = 28
+
+config.font_size = 11
+config.font = wezterm.font("Aporetic Serif Mono")
+config.color_scheme = "GruvboxDarkHard"
+
+config.scrollback_lines = 8192
+config.enable_scroll_bar = true
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+    config.default_prog = { 'C:/Users/hohe/scoop/shims/pwsh.exe', '-NoLogo' }
+end
+
+return config
