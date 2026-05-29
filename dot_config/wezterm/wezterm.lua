21007bf363d92ef62bd31687ea662565719a7a3b
Herbsi <git@hoehenberger.ch>
2026-05-29 22:44:00 +0200

    wezterm: Only use Aporetic Serif Mono font on Windows

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,7 +5,6 @@ config.initial_cols = 120
 config.initial_rows = 28
 
 config.font_size = 11
-config.font = wezterm.font("Aporetic Serif Mono")
 config.color_scheme = "Modus-Vivendi"
 
 config.scrollback_lines = 8192
@@ -15,6 +14,7 @@ config.mux_enable_ssh_agent = false
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     config.default_prog = { 'C:/Users/hohe/scoop/shims/pwsh.exe', '-NoLogo' }
+    config.font = wezterm.font("Aporetic Serif Mono")
 end
 
 return config
