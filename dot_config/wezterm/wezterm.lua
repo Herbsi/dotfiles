620f0c4252dc638e7c8bc67b0afb2f614695876c
Herbsi <git@hoehenberger.ch>
2026-06-16 10:02:06 +0200

    wezterm: Enable kitty keyboard

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.initial_cols = 120
@@ -14,9 +14,12 @@ config.mux_enable_ssh_agent = false
 
 config.font = wezterm.font("Triplicate A Code")
 
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    config.default_prog = { 'C:/Users/hohe/scoop/shims/pwsh.exe', '-NoLogo' }
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.default_prog = { "C:/Users/hohe/scoop/shims/pwsh.exe", "-NoLogo" }
     config.font = wezterm.font("Aporetic Serif Mono")
+    config.allow_win32_input_mode = false
 end
+
+config.enable_kitty_keyboard = true
 
 return config
