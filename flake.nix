334eddb4eef9f406ffe7fa6a5e980fd69df39b50
Herbsi <git@hoehenberger.ch>
2026-06-19 20:30:22 +0200

    Formatting

--- flake.nix
+++ flake.nix
@@ -3,14 +3,21 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
   };
-  outputs = inputs@{ self, nixpkgs, agenix, home-manager, ... }: {
-    nixosConfigurations.kuro = nixpkgs.lib.nixosSystem {
-      modules = [ 
-        ./hardware-configuration.nix
-        ./configuration.nix
-        agenix.nixosModules.default
-      ];
-    }; 
-  };
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      agenix,
+      home-manager,
+      ...
+    }:
+    {
+      nixosConfigurations.kuro = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+          agenix.nixosModules.default
+        ];
+      };
+    };
 }
-
