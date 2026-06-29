7bfc690fe51304da2ede975a8c6afa6ed19c2104
Herbsi <git@hoehenberger.ch>
2026-06-29 08:57:34 +0200

    zellij: Setup Windows config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -13,13 +13,15 @@ config.enable_scroll_bar = true
 config.mux_enable_ssh_agent = false
 
 config.font = wezterm.font("Triplicate A Code")
+config.default_prog = { "C:/Users/hohe/scoop/shims/zellij.exe" }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    config.default_prog = { "C:/Users/hohe/scoop/shims/pwsh.exe", "-NoLogo" }
     config.font = wezterm.font("Aporetic Serif Mono")
     config.allow_win32_input_mode = false
 end
 
 config.enable_kitty_keyboard = true
+
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
